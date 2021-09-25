5ee5551d6575ff4233fbea957801b89f55f9ef08
Jtobyy <jtobi8161@gmail.com>
2021-09-25 22:26:09 +0000

    check for file extension .m

--- monty_main.c
+++ monty_main.c
@@ -11,13 +11,20 @@ int main(int argc, char *argv[])
 char *monty_file;
 int f;
 int c;
+int len;
 if (argc != 2)
 {
-  /*write(STDERR_FILENO, "USAGE: monty file\n", 18);*/
+/*write(STDERR_FILENO, "USAGE: monty file\n", 18);*/
 fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
 monty_file = argv[1];
+len = strlen(monty_file);
+if (monty_file[len - 2] != '.' && monty_file[len - 1] != 'm')
+{
+fprintf(stderr, "Error: Can't open file %s\n", monty_file);
+exit(EXIT_FAILURE);
+}   
 f = open(monty_file, O_RDONLY);
 if (f == -1)
 {
